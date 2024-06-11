33ee2b03c99b62a7fbed7e70f7e2fd339128018b
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-11 10:50:02 +0530

    create library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use multithreaded_webserver_rust::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -8,10 +9,11 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         //println!("Connection established: {}", stream.peer_addr().unwrap());
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
